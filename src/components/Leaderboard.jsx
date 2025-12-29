7d1431d62986344e3accc71ee2fae43c758c8631
Daniel Savage <danielsavage@Mac.lan>
2025-12-29 11:09:26 -0800

    style: Make leaderboard graphic 2x bigger (h-16)

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -304,7 +304,7 @@ export function Leaderboard({ isVisible = true }) {
           <img 
             src="/MintedMerchLeaderboardGraphic.png" 
             alt="Minted Merch Leaderboard" 
-            className="h-8"
+            className="h-16"
           />
           
           <div className="flex items-center gap-4">
